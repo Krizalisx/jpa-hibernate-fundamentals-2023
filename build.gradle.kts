94a303d7b096d50eedc426200dc38b1c3ce3c87a
Viktor Petrov <viktor.petrov@flatexdegiro.com>
2023-11-12 18:05:53 +0100

    Added hibernate dependency.

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,8 @@ repositories {
 }
 
 dependencies {
+    implementation("org.hibernate.orm:hibernate-core:6.3.1.Final")
+
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
