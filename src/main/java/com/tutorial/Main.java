a951f553ad15863396a1796cf134b825dea4eebf
Viktor Petrov <viktor.petrov@flatexdegiro.com>
2023-11-13 22:18:16 +0100

    Implemented connection using java config class.

--- src/main/java/com/tutorial/Main.java
+++ src/main/java/com/tutorial/Main.java
@@ -1,6 +1,11 @@
 package com.tutorial;
 
+import java.util.HashMap;
+
+import org.hibernate.jpa.HibernatePersistenceProvider;
+
 import com.tutorial.entities.Product;
+import com.tutorial.persistence.CustomPersistenceUnitInfo;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
@@ -9,14 +14,15 @@ import jakarta.persistence.Persistence;
 public class Main {
 
     public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("my-persistence-unit");
+//        EntityManagerFactory emf = Persistence.createEntityManagerFactory("my-persistence-unit");
+        EntityManagerFactory emf = new HibernatePersistenceProvider().createContainerEntityManagerFactory(new CustomPersistenceUnitInfo(), new HashMap());
         EntityManager em = emf.createEntityManager(); // represents the context
 
         try {
             em.getTransaction().begin();
 
             Product product = new Product();
-            product.setName("Beer");
+            product.setName("Milk");
 
             em.persist(product); // add this to the context -> NOT AN INSERT QUERY
 
