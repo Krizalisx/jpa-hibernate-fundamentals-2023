d3e3e7f63ef7fac0e44aa7b2bfffef41cbe07f77
Viktor Petrov <viktor_petrov@epam.com>
2024-01-30 20:52:38 +0100

    Implemented simple criteria API queries.

--- src/main/java/com/tutorial/Main.java
+++ src/main/java/com/tutorial/Main.java
@@ -4,6 +4,11 @@ import com.tutorial.entities.jpql.joins_and_inner_queries.Participant;
 import com.tutorial.persistence.CustomPersistenceUnitInfo;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.JoinType;
+import jakarta.persistence.criteria.Root;
+import java.util.Arrays;
 import java.util.Map;
 import org.hibernate.jpa.HibernatePersistenceProvider;
 
@@ -18,16 +23,30 @@ public class Main {
     public static void main(String[] args) {
         try {
             inTransaction(() -> {
-                // named get all query
-                TypedQuery<Participant> getAll = em.createNamedQuery("getAll", Participant.class);
-                getAll.getResultList().forEach(System.out::println);
 
-                System.out.println();
+                CriteriaBuilder builder = em.getCriteriaBuilder();
 
-                // named get by name query
-                TypedQuery<Participant> getByName = em.createNamedQuery("getByName", Participant.class);
-                getByName.setParameter("name", "Alice");
-                getByName.getResultList().forEach(System.out::println);
+                // select p from Participant p
+                CriteriaQuery<Participant> query = builder.createQuery(Participant.class);
+                Root<Participant> participantRoot = query.from(Participant.class);
+                CriteriaQuery<Participant> cq = query.select(participantRoot);
+                em.createQuery(cq).getResultList().forEach(System.out::println);
+
+                // select p.name from Participant p
+                CriteriaQuery<String> query1 = builder.createQuery(String.class);
+                Root<Participant> stringRoot1 = query1.from(Participant.class);
+                CriteriaQuery<String> cq1 = query1.select(stringRoot1.get("name"));
+                em.createQuery(cq1).getResultList().forEach(System.out::println);
+
+                // select p.name, p.id from Participant p
+                CriteriaQuery<Object[]> query2 = builder.createQuery(Object[].class);
+                Root<Participant> stringRoot2 = query2.from(Participant.class);
+                CriteriaQuery<Object[]> cq2 = query2.multiselect(stringRoot2.get("name"), stringRoot2.get("id"), builder.count(stringRoot2.get("id")))
+                    .orderBy(builder.asc(stringRoot2.get("name")))
+                    .groupBy(stringRoot2.get("name"))
+                    .having(builder.gt(builder.count(stringRoot2.get("id")), 1));
+                em.createQuery(cq2).getResultList().stream().map(Arrays::toString).forEach(System.out::println);
+
 
             });
         } finally {
